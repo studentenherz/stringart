0b247ce22578396b22c91fa0b9c375db313db1c0
Michel Romero <studentenherz@gmail.com>
2025-08-11 17:46:44 -0300

    Add WASM target

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+mod stringart;
+pub mod utils;
+
+#[cfg(target_arch = "wasm32")]
+mod wasm {
+    extern crate console_error_panic_hook;
+    use crate::stringart;
+    use wasm_bindgen::prelude::*;
+
+    #[wasm_bindgen]
+    pub fn generate_stringart(
+        image_data: &[u8],
+        num_points: usize,
+        num_lines: usize,
+        weight: u8,
+    ) -> Result<JsValue, serde_wasm_bindgen::Error> {
+        console_error_panic_hook::set_once();
+
+        serde_wasm_bindgen::to_value(&stringart::generate_stringart(
+            image_data, num_points, num_lines, weight,
+        ))
+    }
+}
